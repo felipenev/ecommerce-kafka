05a64de336835f29acadd212c8dee6defe64d4b1
Felipe Neves <matosfnm@gmail.com>
2021-04-26 16:31:05 -0300

    organizing imports

--- src/main/java/br/com/loja/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/loja/ecommerce/FraudDetectorService.java
@@ -1,15 +1,6 @@
 package br.com.loja.ecommerce;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
 
 /**
  * Consumer for Fraud Service
